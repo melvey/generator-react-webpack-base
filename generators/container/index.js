bcc6061099b1861e96ebeea0b2c49f56719ae4d3
Elvey <Matthew.Elvey@cdu.edu.au>
2016-10-10 15:23:50 +0930

    Include existing components in container prompt

--- generators/container/index.js
+++ generators/container/index.js
@@ -2,18 +2,27 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var fs = require('fs');
 var path = require('path');
 
 module.exports = yeoman.Base.extend({
 	prompting: function () {
+
+		// Get existing components for prompt
+		var componentDir = 'src/components';
+		var components = fs.readdirSync(componentDir).filter(function(file) {
+			return fs.statSync(path.join(componentDir, file)).isDirectory()
+		});
+
 		var prompts = [
 			{
-				type: 'input',
+				type: components.length > 0 ? 'list' : input,
 				name: 'component',
 				message: 'Component to create container of',
+				choices: components
 			},
 			{
-				type: 'config',
+				type: 'confirm',
 				name: 'redux',
 				message: 'Use redux?',
 				default: false
@@ -29,7 +38,7 @@ module.exports = yeoman.Base.extend({
 
 	writing: function () {
 
-		let cleanName = this.props.component.replace(/\s*\b\w/g, function(letter) {
+		var cleanName = this.props.component.replace(/\s*\b\w/g, function(letter) {
 			console.log(letter);
 			return letter.charAt(letter.length - 1).toUpperCase();
 		});
