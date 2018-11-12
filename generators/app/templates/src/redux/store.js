b1f776d781a90c156e82f7e3c5887d6ba829ab7f
Elvey <Matthew.Elvey@cdu.edu.au>
2018-11-12 10:53:24 +0930

    pass redux state from server to client

--- generators/app/templates/src/redux/store.js
+++ generators/app/templates/src/redux/store.js
@@ -5,5 +5,13 @@
 import {createStore} from 'redux';
 import reducer from './reducers/RootReducer';
 
-export default createStore(reducer);
+let preloadedState = {};
+if(typeof(window) !== 'undefined') {
+	preloadedState = window.__PRELOADED_STATE__;
+	Object.getOwnPropertyNames(preloadedState).forEach((prop) => {
+		preloadedState[prop] = preloadedState[prop];
+	});
+}
+
+export default createStore(reducer, preloadedState);
 
