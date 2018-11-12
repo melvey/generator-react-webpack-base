f61124f3c2155d338dad8a8b866e4ba177993029
Elvey <Matthew.Elvey@cdu.edu.au>
2018-11-12 10:55:48 +0930

    remove unecessecary loop over state

--- generators/app/templates/src/redux/store.js
+++ generators/app/templates/src/redux/store.js
@@ -8,9 +8,6 @@ import reducer from './reducers/RootReducer';
 let preloadedState = {};
 if(typeof(window) !== 'undefined') {
 	preloadedState = window.__PRELOADED_STATE__;
-	Object.getOwnPropertyNames(preloadedState).forEach((prop) => {
-		preloadedState[prop] = preloadedState[prop];
-	});
 }
 
 export default createStore(reducer, preloadedState);
